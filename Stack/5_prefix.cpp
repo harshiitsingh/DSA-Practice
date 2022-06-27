2f2275b5e054e30f32a2196269c857de6092bfc3
Harshit Singh <77427143+harshiitsingh@users.noreply.github.com>
2022-06-27 22:48:07 +0530

    stack intro

---
+++ Stack/5_prefix.cpp
@@ -0,0 +1,20 @@
+// Prefix Expression Evaluation
+
+#include <bits/stdc++.h>
+#include<stack>
+#include<math.h>
+using namespace std;
+
+int prefixEvaluation(string s){
+    stack<int> st;
+
+    for(int i = s.length()-1; i>=0; i--){
+        
+    }
+}
+
+int main()
+{
+    
+    return 0;
+}
